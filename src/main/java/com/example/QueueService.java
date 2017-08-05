7b05b859b4205f4f40dffe42c3170138163454e4
dmitryvim <dmitry.mikhailovich@gmail.com>
2017-08-05 10:23:58 +0300

    define queue service interface

--- src/main/java/com/example/QueueService.java
+++ src/main/java/com/example/QueueService.java
@@ -2,20 +2,30 @@ package com.example;
 
 public interface QueueService {
 
-  //
-  // Task 1: Define me.
-  //
-  // This interface should include the following methods.  You should choose appropriate
-  // signatures for these methods that prioritise simplicity of implementation for the range of
-  // intended implementations (in-memory, file, and SQS).  You may include additional methods if
-  // you choose.
-  //
-  // - push
-  //   pushes a message onto a queue.
-  // - pull
-  //   retrieves a single message from a queue.
-  // - delete
-  //   deletes a message from the queue that was received by pull().
-  //
+    //
+    // Task 1: Define me.
+    //
+    // This interface should include the following methods.  You should choose appropriate
+    // signatures for these methods that prioritise simplicity of implementation for the range of
+    // intended implementations (in-memory, file, and SQS).  You may include additional methods if
+    // you choose.
+    //
+    // - push
+    //   pushes a message onto a queue.
+    // - pull
+    //   retrieves a single message from a queue.
+    // - delete
+    //   deletes a message from the queue that was received by pull().
+    //
+
+    public void push(String queueName, QueueMessage message);
+
+    public QueueMessage pull(String queueName);
+
+    public void delete(String queueName, QueueMessage message);
+
+    public void createQueue(String name);
+
+    public void deleteQueue(String name);
 
 }
